6cfae70fa7199ef94381c9984c924dc0ec5f60c2
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-15 13:15:24 +0200

    saving stores and using mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,12 @@
 exports.homePage = (req, res) => {
     console.log(req.name);
-    res.render('index')
+    res.render('layout')
 }
 
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add Store'})
+}
+
+exports.createStore = (req, res) => {
+    res.json(req.body)
+}
