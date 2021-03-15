768f43ff84050fe82f9074518163d186d645d5fb
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-15 14:55:37 +0200

    added editing flow for stores, edit storeForm tempate to reflect data from individual store (if any)

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,3 +15,17 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find()
     res.render('stores', { title: 'Stores', stores})
 }
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({ _id: req.params.id})
+    res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+        new: true, //return new store instead of old one
+        runValidators: true //validates input defined in model
+    }).exec(); //force execution
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store`)
+    res.redirect(`/stores/${store._id}/edit`);
+}
