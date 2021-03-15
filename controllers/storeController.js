b14e8d408b1e58442a53aa85d4d64f212ed06292
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-15 14:24:45 +0200

    add getStore controlles to show all available stores, add stores template, add storeCard template for individual store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store');
 
-exports.homePage = (req, res) => {
-    res.render('layout')
-}
-
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'})
 }
@@ -13,4 +9,9 @@ exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Create ${store.name}. Care to leave a review?`);
     res.redirect(`store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find()
+    res.render('stores', { title: 'Stores', stores})
 }
