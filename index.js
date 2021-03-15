92df5401f538b4dd88323cbd7a4dd79ee519f027
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-15 11:59:02 +0200

    connect store controller to the route

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const storeController = require('./controller')
-
-router.get('/', storeController.homePage);
-
-module.exports = router;
