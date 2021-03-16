beaa452ad01d3106660d8e546fc32d36b8fb29ff
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-16 11:54:43 +0200

    custom mongodb aggregations to select tags and their count

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -69,3 +69,9 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store`)
     res.redirect(`/stores/${store._id}/edit`);
 }
+
+exports.getStoresByTag = async (req, res) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag
+    res.render('tag', {tags, title: 'Tags', tag})
+}
