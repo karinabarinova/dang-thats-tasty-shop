a7694c069757f32189f60f060929f5be24dc505e
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-16 15:49:28 +0200

    virtual fields with gravatar for logged in user, protecting addStore route

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,4 +11,13 @@ exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'You are now logged out!')
     res.redirect('/')
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+        return;
+    }
+    req.flash('error', 'Opps! You must be logged in to do that!');
+    res.redirect('/login');
 }
